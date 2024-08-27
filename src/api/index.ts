b36dbbed3297d1b6fe133c89ad19d9314a37c2c9
seoye0ng <bsy0313@gmail.com>
2024-08-27 15:15:50 +0900

    feat: axios typescript 적용(#3)

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,21 +1,29 @@
 /* eslint-disable no-console */
-import axios from 'axios';
+import axios, {
+  Axios,
+  AxiosError,
+  AxiosResponse,
+  InternalAxiosRequestConfig,
+} from 'axios';
 
 // axios 인스턴스 생성
-const instance = axios.create({
+const instance: Axios = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
   withCredentials: true,
 });
 
 // 요청 인터셉터 추가
 instance.interceptors.request.use(
-  (config) => {
-    /* 요청 전 수행할 작업 (예: 인증 헤더 추가 등) */
-    // const access_token = getCookie('access_token');
-    // config.headers.Authorization = `Bearer ${access_token}`;
+  (config: InternalAxiosRequestConfig): InternalAxiosRequestConfig => {
+    // axios 버전이 오르면서 intercepters request 타입이 'AxiosRequestConfig'가 아닌 'InternalAxiosRequestConfig'로 바뀜
+    /*
+     * 요청 전 수행할 작업 (예: 인증 헤더 추가 등)
+     * const access_token = getCookie('access_token');
+     * config.headers.Authorization = `Bearer ${access_token}`;
+     */
     return config;
   },
-  (error) => {
+  (error: AxiosError) => {
     // 요청 에러 시 수행할 작업
     return Promise.reject(error);
   },
@@ -23,11 +31,11 @@ instance.interceptors.request.use(
 
 // 응답 인터셉터 추가
 instance.interceptors.response.use(
-  (response) => {
+  (response: AxiosResponse) => {
     // 응답 성공 시 처리
     return response;
   },
-  (error) => {
+  (error: AxiosError) => {
     const status = error.response?.status;
     // 응답 오류 처리
     if (status === 401) {
