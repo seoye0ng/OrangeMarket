f27305c23c2e8ce744e4016be77e508560aab007
seoye0ng <bsy0313@gmail.com>
2024-09-04 01:00:38 +0900

    feat: NextUiProvider 생성(#4)

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,7 +1,12 @@
+import NextUiProvider from './NextUiProvider';
 import QueryProvider from './QueryProvider';
 
 function Provider({ children }: { children: React.ReactNode }) {
-  return <QueryProvider>{children}</QueryProvider>;
+  return (
+    <QueryProvider>
+      <NextUiProvider>{children}</NextUiProvider>
+    </QueryProvider>
+  );
 }
 
 export default Provider;
