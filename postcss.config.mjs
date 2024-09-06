ea44ae7ee4fe0d820e22d1dd33003bebb15c16e6
seoye0ng <bsy0313@gmail.com>
2024-09-06 19:13:19 +0900

    fix: Cannot find module '@nextui-org/postcss'오류 해결(#4)

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -3,8 +3,6 @@ const config = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {}, // 크로스 브라우저 호환성 추가
-    'postcss-import': {}, // CSS 파일 간 import 기능 지원
-    '@nextui-org/postcss': {}, // Next UI 스타일 플러그인 추가
   },
 };
 
