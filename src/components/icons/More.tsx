f3d594c8fc0f325c51a13ad56323cd6407b6a179
seoye0ng <bsy0313@gmail.com>
2024-09-19 14:01:06 +0900

    feat: More icon 컴포넌트 생성(#5)

---
+++ src/components/icons/More.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import tokens from '@/styles/token';
+
+interface MoreProps {
+  width?: number;
+  color?:
+    | 'primary-50'
+    | 'primary-100'
+    | 'gray-50'
+    | 'gray-100'
+    | 'gray-200'
+    | 'gray-300'
+    | 'black';
+  onClick?: () => void;
+}
+
+function More({ width = 24, color = 'gray-300', onClick }: MoreProps) {
+  return (
+    <svg
+      width={width}
+      height={width}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      onClick={onClick}
+    >
+      <path
+        d="M12 13C12.5523 13 13 12.5523 13 12C13 11.4477 12.5523 11 12 11C11.4477 11 11 11.4477 11 12C11 12.5523 11.4477 13 12 13Z"
+        fill={tokens.colors[color]}
+        stroke={tokens.colors[color]}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M12 6C12.5523 6 13 5.55228 13 5C13 4.44772 12.5523 4 12 4C11.4477 4 11 4.44772 11 5C11 5.55228 11.4477 6 12 6Z"
+        fill={tokens.colors[color]}
+        stroke={tokens.colors[color]}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M12 20C12.5523 20 13 19.5523 13 19C13 18.4477 12.5523 18 12 18C11.4477 18 11 18.4477 11 19C11 19.5523 11.4477 20 12 20Z"
+        fill={tokens.colors[color]}
+        stroke={tokens.colors[color]}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default More;
