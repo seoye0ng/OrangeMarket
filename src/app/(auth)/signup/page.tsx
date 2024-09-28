c241badcadd04a953534beeadf73c7ebc8e482f1
seoye0ng <bsy0313@gmail.com>
2024-09-28 15:28:05 +0900

    fix: 회원가입 페이지 수정(#8)

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,11 +1,11 @@
-import SignupForm from '@/components/common/form/signup-form/SignupForm';
+import AuthForm from '@/components/common/form/auth-form/AuthForm';
 import Title from '@/components/common/title/Title';
 
 export default function page() {
   return (
     <div className="w-full px-34px">
       <Title padding="pt-30px pb-10" title="이메일로 회원가입" />
-      <SignupForm />
+      <AuthForm />
     </div>
   );
 }
