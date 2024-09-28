22985aadf3b5be256afc78dfd878da35313ee864
seoye0ng <bsy0313@gmail.com>
2024-09-28 15:19:54 +0900

    feat: login 페이지 ui 제작(#10)

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import AuthForm from '@/components/common/form/auth-form/AuthForm';
+import Title from '@/components/common/title/Title';
+
+export default function page() {
+  return (
+    <div className="w-full px-34px">
+      <Title padding="pt-30px pb-10" title="로그인" />
+      <AuthForm />
+    </div>
+  );
+}
