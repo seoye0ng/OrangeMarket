44efa156e3aa61cb1666e46a80727f2a290c0fcd
seoye0ng <bsy0313@gmail.com>
2024-10-02 17:53:29 +0900

    fix: AuthForm 디자인 수정 및 login, signup에 공통으로 사용되는 input field를 authField 배열로 관리하여 중복 코드 제거(#8, #10)

--- src/components/common/form/auth-form/AuthForm.tsx
+++ src/components/common/form/auth-form/AuthForm.tsx
@@ -5,34 +5,30 @@ interface AuthFormProps {
   type: 'login' | 'signup';
 }
 
+const authFields = [
+  { type: 'email', label: '이메일', placeholder: '이메일을 입력하세요.' },
+  {
+    type: 'password',
+    label: '비밀번호',
+    placeholder: '비밀번호를 입력하세요.',
+  },
+];
+
 export default function AuthForm({ type = 'signup' }: AuthFormProps) {
   return (
     <form className="flex-center gap-30px">
       <div className="flex-center gap-4">
         {/* 에러 시 isInvalid  ture */}
-        <UnderlineInput
-          isClearable
-          variant="underlined"
-          type="email"
-          label="이메일"
-          placeholder="이메일을 입력하세요."
-        />
-        <UnderlineInput
-          isClearable
-          variant="underlined"
-          type="password"
-          label="비밀번호"
-          placeholder="비밀번호를 입력하세요."
-        />
-        {type === 'signup' && (
+        {authFields.map((field) => (
           <UnderlineInput
+            key={field.type}
             isClearable
             variant="underlined"
-            type="password"
-            label="비밀번호 확인"
-            placeholder="비밀번호를 다시 입력하세요."
+            type={field.type}
+            label={field.label}
+            placeholder={field.placeholder}
           />
-        )}
+        ))}
       </div>
       <CustomButton color="primary" size="l" radius="full">
         {type === 'signup' ? '다음' : '로그인'}
