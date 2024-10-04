55e586cd3b08d5044a35af4cbc854c39cad24349
seoye0ng <bsy0313@gmail.com>
2024-10-05 00:53:55 +0900

    feat: 프로필 설정에서 뒤로가기 시 이전 step으로 되돌아가도록 구현(#8, #17)

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -4,7 +4,7 @@ import AuthForm from '@/components/common/form/auth-form/AuthForm';
 import ProfileForm from '@/components/common/form/auth-form/ProfileForm';
 import Title from '@/components/common/title/Title';
 import { titleText } from '@/constants/titleText';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function page() {
   const [step, setStep] = useState(1);
@@ -12,7 +12,28 @@ export default function page() {
   // 다음 버튼을 눌렀을 때 next step으로 이동 (프로필 설정 페이지)
   const handleNext = () => {
     setStep((currentStep) => currentStep + 1);
+    window.history.pushState(null, ''); // 히스토리 스택에 새로운 상태 추가
   };
+
+  // 뒤로 가기 시 이전 step으로 이동 (회원가입 페이지)
+  const handleBack = () => {
+    if (step > 1) {
+      setStep((currentStep) => currentStep - 1);
+    }
+  };
+
+  useEffect(() => {
+    const handlePopState = () => {
+      handleBack();
+    };
+
+    // popstate 이벤트 감지 (브라우저 뒤로 가기 버튼 감지)
+    window.addEventListener('popstate', handlePopState);
+
+    return () => {
+      window.removeEventListener('popstate', handlePopState);
+    };
+  }, [step]);
 
   const titleInfo = titleText[step === 1 ? 'signup' : 'set-profile'];
   const padding = step === 1 ? 'pt-30px pb-10' : 'py-30px';
