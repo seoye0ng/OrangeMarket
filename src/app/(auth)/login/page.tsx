339d0208c97913eff431742c0dd0a00609c6826f
seoye0ng <bsy0313@gmail.com>
2024-10-04 22:18:28 +0900

    design: 로그인 페이지 회원가입 버튼 추가(#10)

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,11 +1,15 @@
+import Link from 'next/link';
 import AuthForm from '@/components/common/form/auth-form/AuthForm';
-import Title from '@/components/common/title/Title';
 
 export default function page() {
   return (
-    <div className="w-full px-34px">
-      <Title padding="pt-30px pb-10" title="로그인" />
+    <>
       <AuthForm type="login" />
-    </div>
+      <Link href="/signup">
+        <button className="mx-auto mt-4 block p-1 text-12px font-normal text-gray-300">
+          이메일로 회원가입
+        </button>
+      </Link>
+    </>
   );
 }
