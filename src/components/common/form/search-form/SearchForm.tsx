2c648f9cf1c1b36092307927d331dbdfa343a350
seoye0ng <bsy0313@gmail.com>
2024-10-09 20:32:17 +0900

    feat: useRef를 이용해 부모컴포넌트에서 값을 참조하도록 구현 및 검색 시에  작동하는 handleSearchSubmit 함수 작성(#22)

--- src/components/common/form/search-form/SearchForm.tsx
+++ src/components/common/form/search-form/SearchForm.tsx
@@ -1,19 +1,24 @@
+'use client';
+
 import { Input } from '@nextui-org/react';
-import { FormEvent } from 'react';
+import { useRef, FormEvent } from 'react';
 
 interface ISearchForm {
   onSearch: (searchTerm: string) => void;
 }
 
 export default function SearchForm({ onSearch }: ISearchForm) {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
   const handleSearchSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    onSearch();
+    const searchTerm = inputRef.current?.value || '';
+    onSearch(searchTerm);
   };
 
   return (
     <form onSubmit={handleSearchSubmit} className="ml-5 flex-grow">
-      <Input radius="full" placeholder="계정 검색" isClearable />
+      <Input radius="full" ref={inputRef} placeholder="계정 검색" isClearable />
     </form>
   );
 }
