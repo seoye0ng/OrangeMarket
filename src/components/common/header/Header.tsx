0477793fdcb5a0fe40e179136f601e01f5ed6fe7
seoye0ng <bsy0313@gmail.com>
2024-10-11 14:21:05 +0900

    refactor: headerConfig에서 header 설정을 관리함으로써 Header컴포넌트는 깔끔하게 유지(#21)

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,85 +1,50 @@
 'use client';
 
-import BackArrow from '@/components/icons/BackArrow';
-import More from '@/components/icons/More';
-import Search from '@/components/icons/Search';
+import { usePathname, useRouter } from 'next/navigation';
+
+import { headerConfig } from '@/config/headerConfig';
 
 import withHeader from './withHeader';
 
-interface IHeaderProps {
-  pageType?: string;
-}
+export default function Header() {
+  const pathname = usePathname();
+  const router = useRouter();
 
-const handleClick = () => {
-  console.log('click');
-};
+  const config = headerConfig[pathname];
 
-const HomeHeader = withHeader('감귤마켓 피드', undefined, () => (
-  <Search activeColor="gray-200" onClick={handleClick} />
-));
-const SearchHeader = withHeader(
-  undefined,
-  () => <BackArrow activeColor="gray-300" onClick={handleClick} />,
-  () => <Search activeColor="gray-200" onClick={handleClick} />,
-);
-const ProfileHeader = withHeader(
-  undefined,
-  () => <BackArrow activeColor="gray-300" onClick={handleClick} />,
-  () => <More activeColor="gray-200" onClick={handleClick} />,
-);
-const ChatListHeader = withHeader(
-  undefined,
-  () => <BackArrow activeColor="gray-300" onClick={handleClick} />,
-  () => <More activeColor="gray-200" onClick={handleClick} />,
-);
-const PostHeader = withHeader(
-  undefined,
-  () => <BackArrow activeColor="gray-300" onClick={handleClick} />,
-  () => <More activeColor="gray-200" onClick={handleClick} />,
-);
-const ChatHeader = withHeader(
-  undefined,
-  () => <BackArrow activeColor="gray-300" onClick={handleClick} />,
-  () => <More activeColor="gray-200" onClick={handleClick} />,
-);
-const FollowerHeader = withHeader(
-  undefined,
-  () => <BackArrow activeColor="gray-300" onClick={handleClick} />,
-  undefined,
-);
-// TODO: More버튼 업로드 버튼으로 바꾸기
-const ProductHeader = withHeader(
-  undefined,
-  () => <BackArrow activeColor="gray-300" onClick={handleClick} />,
-  () => <More activeColor="gray-200" onClick={handleClick} />,
-);
-const EditProfileHeader = withHeader(
-  undefined,
-  () => <BackArrow activeColor="gray-300" onClick={handleClick} />,
-  () => <More activeColor="gray-200" onClick={handleClick} />,
-);
+  if (!config) return null;
 
-export default function Header({ pageType = 'home' }: IHeaderProps) {
-  switch (pageType) {
-    case 'home':
-      return <HomeHeader />;
-    case 'search':
-      return <SearchHeader />;
-    case 'profile':
-      return <ProfileHeader />;
-    case 'chatList':
-      return <ChatListHeader />;
-    case 'post':
-      return <PostHeader />;
-    case 'follower':
-      return <FollowerHeader />;
-    case 'chat':
-      return <ChatHeader />;
-    case 'product':
-      return <ProductHeader />;
-    case 'edit-profile':
-      return <EditProfileHeader />;
-    default:
-      return <HomeHeader />;
-  }
+  const handleBackClick = () => {
+    router.back();
+  };
+
+  const handleRightIconClick = () => {
+    switch (pathname) {
+      case '/':
+        console.log('검색');
+        router.push('/search');
+        break;
+      case '/profile':
+        // openModal();
+        console.log('더보기 모달 open');
+        break;
+      case '/upload':
+        // handleUpload();
+        console.log('업로드');
+        router.push('/');
+        break;
+      default:
+        break;
+    }
+  };
+
+  const DynamicHeader = withHeader(
+    config.title,
+    config.LeftIcon,
+    config.RightIcon,
+    handleBackClick,
+    handleRightIconClick,
+  );
+
+  return <DynamicHeader />;
 }
