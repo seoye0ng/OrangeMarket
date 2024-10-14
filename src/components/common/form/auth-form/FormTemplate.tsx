464826cda4b7dbbac066e5dcd784a1ea5c2cb556
seoye0ng <bsy0313@gmail.com>
2024-10-14 18:53:59 +0900

    fix: 회원가입 시 다음 step으로 넘어갔을 때 자동으로 유효성 검사 오류가 발생하는 버그 해결(#9) FormTemplate컴포넌트에 touchedFields를 추가하여 필드가 터치되었을 때만 유효성 검사 에러 메시지를 보여주도록 수정(스텝이 변경되었을 때, 자동으로 유효성 검사 오류가 발생하지 않도록 제어)

--- src/components/common/form/auth-form/FormTemplate.tsx
+++ src/components/common/form/auth-form/FormTemplate.tsx
@@ -15,31 +15,36 @@ interface FormTemplateProps {
 export default function FormTemplate({ fields, children }: FormTemplateProps) {
   const {
     register,
-    formState: { errors },
+    formState: { errors, touchedFields },
   } = useFormContext();
 
   return (
     <fieldset className="flex-center gap-4">
       {children}
-      {Object.entries(fields).map(([name, field]) => (
-        <UnderlineInput
-          key={name}
-          {...register(name, field.validation)}
-          isClearable
-          variant="underlined"
-          type={
-            name === 'email'
-              ? 'email'
-              : name === 'password'
-                ? 'password'
-                : 'text'
-          }
-          label={field.label}
-          placeholder={field.placeholder}
-          isInvalid={!!errors[name]}
-          errorMessage={errors[name]?.message?.toString()}
-        />
-      ))}
+      {Object.entries(fields).map(([name, field]) => {
+        // 필드가 터치되었을 때만 에러 표시
+        const hasError = touchedFields[name] && errors[name];
+
+        return (
+          <UnderlineInput
+            key={name}
+            {...register(name, field.validation)}
+            isClearable
+            variant="underlined"
+            type={
+              name === 'email'
+                ? 'email'
+                : name === 'password'
+                  ? 'password'
+                  : 'text'
+            }
+            label={field.label}
+            placeholder={field.placeholder}
+            isInvalid={!!hasError}
+            errorMessage={errors[name]?.message?.toString()}
+          />
+        );
+      })}
     </fieldset>
   );
 }
