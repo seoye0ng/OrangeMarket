cb415ad8b881712cde7fd2d63e0eaae91539735f
seoye0ng <bsy0313@gmail.com>
2024-10-15 16:39:58 +0900

    feat: search 페이지 UI 구현(#25)

---
+++ src/app/search/page.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-underscore-dangle */
+
+import { IUserProfileBase } from '@/api/types/user';
+import UserCard from '@/components/common/user-card/UserCard';
+import { userList } from '@/mocks/userList';
+
+export default function Search() {
+  return (
+    <main className="mt-5 px-4">
+      {userList.map((user: IUserProfileBase) => (
+        <section key={user._id} className="mb-5">
+          <UserCard imageSize="42px" userProfile={user} />
+        </section>
+      ))}
+    </main>
+  );
+}
