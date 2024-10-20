37aeee1ad22d3e23de7c039e054e6927540c8215
seoye0ng <bsy0313@gmail.com>
2024-10-21 01:32:59 +0900

    feat: 로그인 api 요청 함수 구현(#11)

--- src/api/apiRequests/auth.ts
+++ src/api/apiRequests/auth.ts
@@ -1,9 +1,23 @@
 import { postRequest } from '@/api/requests';
-import { ISignUpRequest, ISignupResponse } from '@/api/types/auth';
+import {
+  ILoginRequest,
+  ILoginResponse,
+  ISignUpRequest,
+  ISignupResponse,
+} from '@/api/types/auth';
 
 export const signup = async (data: ISignUpRequest) => {
   const response = await postRequest<ISignupResponse, ISignUpRequest>(
     '/user',
+    data,
+  );
+
+  return response;
+};
+
+export const login = async (data: ILoginRequest) => {
+  const response = await postRequest<ILoginResponse, ILoginRequest>(
+    '/user/login',
     data,
   );
 
