8f0bb25436ce96715f510ec91f6437fe633a7e00
seoye0ng <bsy0313@gmail.com>
2024-10-21 00:52:46 +0900

    feat: 로그인 폼에 react-hook-form 적용(#11)

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,29 @@
+'use client';
+
 import Link from 'next/link';
-import AuthForm from '@/components/common/form/auth-form/AuthForm';
+import { FormProvider, useForm } from 'react-hook-form';
+
+import CustomButton from '@/components/common/button/Button';
+import FormTemplate from '@/components/common/form/auth-form/FormTemplate';
 import Title from '@/components/common/title/Title';
+import { loginFields } from '@/config/authFieldConfig';
 import { TITLE_TEXT } from '@/constants/titleText';
 
-export default function page() {
+export default function Login() {
+  const methods = useForm({
+    mode: 'onBlur',
+    defaultValues: { email: '', password: '' },
+  });
+
+  const {
+    handleSubmit,
+    formState: { isValid },
+  } = methods;
+
+  const onSubmit = () => {
+    console.log('성공');
+  };
+
   return (
     <>
       <Title
@@ -11,9 +31,26 @@ export default function page() {
         title={TITLE_TEXT.login.title}
         description={TITLE_TEXT.login.description}
       />
-      <AuthForm type="login" />
+      <FormProvider {...methods}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormTemplate fields={loginFields} />
+          <CustomButton
+            type="submit"
+            color="primary"
+            size="l"
+            radius="full"
+            className="mt-30px"
+            isDisabled={!isValid}
+          >
+            로그인
+          </CustomButton>
+        </form>
+      </FormProvider>
       <Link href="/signup">
-        <button className="mx-auto mt-4 block p-1 text-12px font-normal text-gray-300">
+        <button
+          type="button"
+          className="mx-auto mt-4 block p-1 text-12px font-normal text-gray-300"
+        >
           이메일로 회원가입
         </button>
       </Link>
