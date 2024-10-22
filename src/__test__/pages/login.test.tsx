518c87b3e0c203837f324587a8b272d089509003
seoye0ng <bsy0313@gmail.com>
2024-10-22 23:49:14 +0900

    feat: 로그인 폼 인풋 필드 값 입력 테스트 구현(Next-Study-habjung#11)

--- src/__test__/pages/login.test.tsx
+++ src/__test__/pages/login.test.tsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import Login from '@/app/(auth)/login/page';
@@ -10,7 +10,7 @@ jest.mock('next/navigation', () => ({
 }));
 
 describe('Login', () => {
-  it('로그인 헤더 타이틀 렌더링', () => {
+  it('로그인 페이지 요소 렌더링', () => {
     // QueryClient 생성
     const queryClient = new QueryClient();
 
@@ -21,8 +21,35 @@ describe('Login', () => {
       </QueryClientProvider>,
     );
     const heading = screen.getByRole('heading', { level: 1 });
+    const emailInput = screen.getByLabelText('이메일');
+    const passwordInput = screen.getByLabelText('비밀번호');
+    const loginButton = screen.getByRole('button', { name: '로그인' });
 
     expect(heading).toBeInTheDocument();
     expect(heading).toHaveTextContent('로그인');
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(loginButton).toBeInTheDocument();
+  });
+
+  it('로그인 폼 인풋 필드 값 입력', () => {
+    const queryClient = new QueryClient();
+
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Login />
+      </QueryClientProvider>,
+    );
+
+    const emailInput = screen.getByPlaceholderText('이메일을 입력하세요.');
+    const passwordInput = screen.getByPlaceholderText('비밀번호를 입력하세요.');
+
+    // 인풋 필드에 값 입력
+    fireEvent.change(emailInput, { target: { value: 'test123@example.com' } });
+    fireEvent.change(passwordInput, { target: { value: 'password123!' } });
+
+    // 입력 필드의 값이 변경되었는지 확인
+    expect(emailInput).toHaveValue('test123@example.com');
+    expect(passwordInput).toHaveValue('password123!');
   });
 });
