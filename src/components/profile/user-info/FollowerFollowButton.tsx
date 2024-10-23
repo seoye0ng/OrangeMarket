2e840839885f33e0e406b5f96eede8f9d9bf3eaa
seoye0ng <bsy0313@gmail.com>
2024-10-24 04:27:34 +0900

    feat: follower, following count가 만이 넘으면 소수점 둘째자리에서 반올림해주는 함수 구현(Next-Study-habjung#18)

--- src/components/profile/user-info/FollowerFollowButton.tsx
+++ src/components/profile/user-info/FollowerFollowButton.tsx
@@ -1,3 +1,5 @@
+import { formatFollowerCount } from '@/utils/fomatFollowCount';
+
 interface IFollowerFollowingButtonProps {
   count: number;
   onClick?: () => void;
@@ -8,8 +10,8 @@ export const FollowerFollowingButton = ({
   onClick,
 }: IFollowerFollowingButtonProps) => {
   return (
-    <button type="button" className="max-w-12 text-center" onClick={onClick}>
-      <p className="mb-6px font-bold">{count}</p>
+    <button type="button" className="text-center" onClick={onClick}>
+      <p className="mb-6px font-bold">{formatFollowerCount(count)}</p>
       <p className="text-10px font-normal leading-3 text-gray-300">followers</p>
     </button>
   );
