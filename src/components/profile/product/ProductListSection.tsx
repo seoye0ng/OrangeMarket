4153daa9f346ce8a3e34b3296b22ad6a267702bb
seoye0ng <bsy0313@gmail.com>
2024-10-23 17:39:57 +0900

    refactor: ProductCard컴포넌트에서 onClick을 props로 받도록 수정 및 옵셔널 체이닝 삭제(Next-Study-habjung#18) 상위 컴포넌트에서 상태 관리나 공통된 동작(모달 띄우기 등)이 필요할 때, 클릭 이벤트를 ProductListSection에 정의하는 것이 더 적합하다고 판단(일관성있게 데이터를 관리하고 흐름을 상위에서 통제할 수 있음)

--- src/components/profile/product/ProductListSection.tsx
+++ src/components/profile/product/ProductListSection.tsx
@@ -1,29 +1,39 @@
 import classNames from 'classnames';
 
-import { productList } from '@/mocks/productList';
-
 import ProductCard from './ProductCard';
+import { IProduct, IProductList } from '@/api/types/product';
 
 interface IProductListSectionProps {
+  productList: IProductList;
   className?: string;
 }
 
 export default function ProductListSection({
+  productList,
   className,
 }: IProductListSectionProps) {
-  const { product, data } = productList;
+  const { product } = productList;
+
+  // TODO: 상품 클릭시 상품 상세페이지 이동 or 상품 모달
+  const handleProductClick = (productData: IProduct) => {
+    console.log(`click: ${productData.itemName}`);
+  };
 
   return (
-    <section className={classNames('py-5', className)}>
+    <section className={classNames(className)}>
       <p className="mx-16px mb-4 text-16px font-bold leading-17px">
         판매중인 상품
       </p>
-      {data === 0 ? (
+      {product.length === 0 ? (
         <div className="mx-16px">현재 판매중인 상품이 없습니다.</div>
       ) : (
         <div className="horizontal-scroll scrollbar-hidden flex gap-15px px-4">
           {product.map((productData) => (
-            <ProductCard key={productData.id} product={productData} />
+            <ProductCard
+              key={productData.id}
+              product={productData}
+              onClick={() => handleProductClick(productData)}
+            />
           ))}
         </div>
       )}
