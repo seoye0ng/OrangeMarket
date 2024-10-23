87ec839f57d4e37b5a70e58e55c5b3774bb1a7a1
seoye0ng <bsy0313@gmail.com>
2024-10-24 03:59:49 +0900

    refactor: postType에 따라 리스트형 포스트와 앨범형 포스트를 각각 관리하는 컴포넌트로 분리하여 명확한 역할 분리(Next-Study-habjung#18)

--- src/components/profile/post/PostSection.tsx
+++ src/components/profile/post/PostSection.tsx
@@ -4,9 +4,8 @@ import classNames from 'classnames';
 import { useState } from 'react';
 
 import { IPostList } from '@/api/types/post';
-import { PostContent } from '@/components/common/post-item/post-card';
 import { IconSwitcher } from './IconSwitcher';
-import PostItem from '@/components/common/post-item/PostItem';
+import { PostAlbumView, PostListView } from './PostView';
 
 interface IPostSection {
   className?: string;
@@ -23,22 +22,8 @@ export default function PostSection({ className, postList }: IPostSection) {
         onListClick={() => setPostType('list')}
         onAlbumClick={() => setPostType('album')}
       />
-      {postType === 'list' && (
-        <div className="px-4 pb-20 pt-4">
-          {postList.post.map(({ author, ...post }) => (
-            <PostItem author={author} post={post} />
-          ))}
-        </div>
-      )}
-      {postType === 'album' && (
-        <div className="grid grid-cols-3 gap-2 pb-20 pt-4 shadow-test1">
-          {postList.post
-            .filter((post) => post.image)
-            .map(({ author, ...post }) => (
-              <PostContent isListType={false} image={post.image} />
-            ))}
-        </div>
-      )}
+      {postType === 'list' && <PostListView postList={postList} />}
+      {postType === 'album' && <PostAlbumView postList={postList} />}
     </section>
   );
 }
