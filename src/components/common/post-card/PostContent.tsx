7a05362bd3bfc8be5144e3aac7199bb5878d913f
seoye0ng <bsy0313@gmail.com>
2024-10-24 02:07:49 +0900

    feat: PostContent를 조건부 스타일 적용 및 post-card폴더 index파일 생성(Next-Study-habjung#18) 조건부 스타일로 재사용가능하도록 구현, 해당 폴더 내 모든 컴포넌트를 한 번에 import/export할 수 있도록 index파일 추가

--- src/components/common/post-card/PostContent.tsx
+++ src/components/common/post-card/PostContent.tsx
@@ -1,26 +1,37 @@
+import classNames from 'classnames';
 import Image from 'next/image';
 
 import { IPost } from '@/api/types/post';
 
 interface IPostContentProps {
-  content: IPost['content'];
-  image: IPost['image'];
+  isListType?: boolean;
+  content?: IPost['content'];
+  image?: IPost['image'];
 }
 
-export default function PostContent({ content, image }: IPostContentProps) {
+export default function PostContent({
+  isListType = true,
+  content,
+  image,
+}: IPostContentProps) {
   return (
     <>
-      <div className="my-4 text-14px font-normal leading-17px">{content}</div>
+      {content && (
+        <div className="my-4 text-14px font-normal leading-17px">{content}</div>
+      )}
       {image && (
-        <div className="rounded-10px relative aspect-[4/3] max-h-[228px] w-full overflow-hidden">
+        <div
+          className={classNames('relative overflow-hidden rounded-10px', {
+            'aspect-[4/3] max-h-[228px] w-full': isListType,
+            'aspect-square': !isListType,
+          })}
+        >
           <Image
             src={image}
             sizes="100vw"
             alt="이미지"
             fill
-            style={{
-              objectFit: 'cover',
-            }}
+            className="object-cover"
           />
         </div>
       )}
---
+++ src/components/common/post-card/index.ts
@@ -0,0 +1,3 @@
+export { default as PostCard } from './PostCard';
+export { default as PostContent } from './PostContent';
+export { default as PostReaction } from './PostReaction';
