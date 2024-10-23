76be7d2c8c9137576649f1fec94a15b95ccf13c2
seoye0ng <bsy0313@gmail.com>
2024-10-24 03:16:39 +0900

    refactor: IconSwitcher로 컴포넌트 분리 및 중복 코드 map함수로 처리(Next-Study-habjung#18)

--- src/components/profile/post/PostSection.tsx
+++ src/components/profile/post/PostSection.tsx
@@ -7,47 +7,24 @@ import { IPostList } from '@/api/types/post';
 import IconButton from '@/components/common/button/IconButton';
 import { PostCard, PostContent } from '@/components/common/post-card';
 import UserCard from '@/components/common/user-card/UserCard';
-import PostList from '@/components/icons/PostList';
-import PostAlbum from '@/components/icons/PostAlbum';
 import SMore from '@/components/icons/SMore';
-import { IconColor } from '@/styles/token';
+import { IconSwitcher } from './IconSwitcher';
 
 interface IPostSection {
   className?: string;
   postList: IPostList;
 }
 
-const getIconColor = (currentType: string, targetType: string): IconColor => {
-  return currentType === targetType ? 'gray-300' : 'gray-100';
-};
-
 export default function PostSection({ className, postList }: IPostSection) {
-  const [postType, setPostType] = useState('list');
-
-  const handleListClick = () => {
-    setPostType('list');
-  };
-
-  const handleAlbumClick = () => {
-    setPostType('album');
-  };
+  const [postType, setPostType] = useState<'list' | 'album'>('list');
 
   return (
     <section className={classNames('', className)}>
-      <div className="border-b-1 border-gray-100 px-4 pt-2 text-right">
-        <IconButton
-          className="ml-4"
-          label="리스트형 아이콘"
-          onClick={handleListClick}
-          icon={<PostList color={getIconColor(postType, 'list')} />}
-        />
-        <IconButton
-          className="ml-4"
-          label="앨범형 아이콘"
-          onClick={handleAlbumClick}
-          icon={<PostAlbum color={getIconColor(postType, 'album')} />}
-        />
-      </div>
+      <IconSwitcher
+        postType={postType}
+        onListClick={() => setPostType('list')}
+        onAlbumClick={() => setPostType('album')}
+      />
       {postType === 'list' && (
         <div className="px-4 pb-20 pt-4">
           {postList.post.map(({ author, ...post }) => (
