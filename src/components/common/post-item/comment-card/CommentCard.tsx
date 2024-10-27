0feebe47c45ac609a994ef83e5e3ed56fd188a4b
seoye0ng <bsy0313@gmail.com>
2024-10-27 13:52:46 +0900

    feat: day.js 적용해서 날짜 계산((Next-Study-habjung#28)

--- src/components/common/post-item/comment-card/CommentCard.tsx
+++ src/components/common/post-item/comment-card/CommentCard.tsx
@@ -1,19 +1,33 @@
 import classNames from 'classnames';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
-import { IUserProfile, IUserProfileBase } from '@/api/types/user';
+import { ICommentWithoutAuthor } from '@/api/types/comment';
+import { IUserProfile } from '@/api/types/user';
 import IconButton from '@/components/common/button/IconButton';
 import SMore from '@/components/icons/SMore';
 
+import 'dayjs/locale/ko';
 import UserImage from '../user-card/UserImage';
-import { ICommentWithoutAuthor } from '@/api/types/comment';
 
-interface ICommentProps {
+dayjs.extend(relativeTime);
+dayjs.locale('ko');
+
+interface ICommentCardProps {
   user: IUserProfile;
   comment: ICommentWithoutAuthor;
   className?: string;
 }
 
-export default function Comment({ user, comment, className }: ICommentProps) {
+const getTimeAgo = (createdAt: string) => {
+  return dayjs(createdAt).fromNow();
+};
+
+export default function CommentCard({
+  user,
+  comment,
+  className,
+}: ICommentCardProps) {
   return (
     <article className={classNames('flex gap-3', className)}>
       <UserImage user={user} size="36px" />
@@ -21,8 +35,7 @@ export default function Comment({ user, comment, className }: ICommentProps) {
         <div className="flex gap-6px">
           <p className="text-14px font-medium leading-17px">{user.username}</p>
           <span className="font-nomal text-10px leading-17px text-gray-300">
-            {/* TODO: 24시간 이후부터는 날짜 렌더/ 1시간 이상부터는 분 단위 제거 */}
-            {`· ${comment.createdAt}`}
+            {`· ${getTimeAgo(comment.createdAt)}`}
           </span>
           <IconButton icon={SMore} className="ml-auto" label="더보기 버튼" />
         </div>
