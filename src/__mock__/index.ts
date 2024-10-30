a050ed04d2768fb6a03203b73bb4a04228152435
seoye0ng <bsy0313@gmail.com>
2024-10-31 04:30:47 +0900

    feat: 게시물 상세 api 요청 모킹 테스트 함수 구현(Next-Study-habjung#39)

--- src/__mock__/index.ts
+++ src/__mock__/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import MockAdapter from 'axios-mock-adapter';
 
 import instance from '@/api/index'; // axios 인스턴스 가져오기
@@ -20,4 +21,27 @@ mock.onGet(/\/post\/feed.*/).reply((config) => {
   const posts = postListData.posts.slice(skip, skip + limit);
 
   return [200, { posts }];
+});
+
+mock.onGet(/\/post\/:\d+/).reply((config) => {
+  const { url } = config;
+
+  // url이 없을 경우 에러 처리
+  if (!url) {
+    return [400, { message: '잘못된 요청입니다.' }];
+  }
+
+  const urlParts = url.split('/');
+
+  // 마지막 부분에서 ':'가 포함된 숫자 가져오기
+  const lastPart = urlParts[urlParts.length - 1]; // ':1'과 같은 형식
+  const postId = lastPart.substring(1); // ':'를 제거하고 숫자만 가져옴
+
+  // mock 데이터에서 postId에 해당하는 포스트 찾기
+  const post = postListData.posts.find((p) => p.id === postId);
+
+  if (post) {
+    return [200, { post }]; // 포스트가 있는 경우
+  }
+  return [404, { message: '존재하지 않는 게시글입니다.' }]; // 포스트가 없는 경우
 });
