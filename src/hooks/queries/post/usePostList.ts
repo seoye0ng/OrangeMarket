7fb8e840d2d913a78e758a8915da3c8215574df3
seoye0ng <bsy0313@gmail.com>
2024-10-30 18:05:09 +0900

    feat: useInfiniteQuery훅을 사용해 무한스크롤 구현할 수 있도록 useInfinitePostList 쿼리훅 구현(Next-Study-habjung#30)

--- src/hooks/queries/post/usePostList.ts
+++ src/hooks/queries/post/usePostList.ts
@@ -1,12 +1,27 @@
-import { useQuery } from '@tanstack/react-query';
+import { keepPreviousData, useInfiniteQuery } from '@tanstack/react-query';
 
 import { postList } from '@/api/apiRequests/post';
+import { IPostList } from '@/api/types/post';
 
-function usePostList(limit: number, skip: number) {
-  return useQuery({
-    queryKey: ['postList', limit, skip],
-    queryFn: () => postList(limit, skip),
+function useInfinitePostList(limit: number) {
+  return useInfiniteQuery<IPostList>({
+    queryKey: ['postList', limit],
+    queryFn: ({ pageParam = 0 }) => {
+      return postList(limit, pageParam as number); // pageParam을 사용하여 skip 관리
+    },
+    getNextPageParam: (lastPage, allPages) => {
+      // 마지막 페이지에서 받은 게시물 수가 limit보다 작으면 다음 페이지가 없으므로 undefined 반환
+      if (lastPage.posts.length < limit) {
+        return undefined;
+      }
+      // 다음 페이지가 있을 경우 pageParam(skip)을 계산하여 반환
+      const nextPageParam = allPages.length * limit; // 다음 페이지의 skip 값으로 설정
+      return nextPageParam;
+    },
+    initialPageParam: 0, // 초기 페이지 파라미터를 0으로 설정
+    staleTime: Infinity,
+    placeholderData: keepPreviousData,
   });
 }
 
-export default usePostList;
+export default useInfinitePostList;
