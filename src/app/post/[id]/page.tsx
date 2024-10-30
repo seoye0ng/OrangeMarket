42ed8ad3153422943c752a6505ae40cb76ab3321
seoye0ng <bsy0313@gmail.com>
2024-10-31 04:32:04 +0900

    feat: 게시물 상세 쿼리훅 호출(Next-Study-habjung#39)

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,17 +1,22 @@
 'use client';
 
+import '@/__mock__/';
 import { commentList } from '@/__mock__/commentList';
-import { postList } from '@/__mock__/postList';
 import CommentForm from '@/components/common/form/comment-form/CommentForm';
 import CommentCard from '@/components/common/post-item/comment-card/CommentCard';
 import PostItem from '@/components/common/post-item/PostItem';
+import usePostDetail from '@/hooks/queries/post/usePostDetail';
 
-const { author, ...post } = postList.post[0];
-const { comments } = commentList;
+interface PostDetailPageProps {
+  params: { id: string };
+}
 
-export default function PostDetailsPage() {
-  /* TODO: 게시글을 불러오는 쿼리 훅을 연결해 `post` 데이터를 가져옴.
-  post.commentCount가 1 이상인지 확인해서, 댓글이 있을 때만 comment 쿼리 훅을 실행하도록 설정. */
+export default function PostDetailsPage({ params }: PostDetailPageProps) {
+  const { id } = params; // URL의 id 파라미터 추출
+
+  const { data: postDetail, isLoading, error } = usePostDetail(id);
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
 
   const handleComment = (commentTerm: string) => {
     console.log('input ref', commentTerm);
@@ -20,24 +25,28 @@ export default function PostDetailsPage() {
   };
 
   return (
-    <main className="relative pb-20 pt-5">
-      <PostItem author={author} post={post} className="px-4" />
-      {post.commentCount > 0 && (
-        <section className="border-t-1 border-gray-100 px-4 pt-5">
-          {comments.map(({ author: commentAuthor, ...comment }) => (
-            <CommentCard
-              key={comment.id}
-              comment={comment}
-              user={commentAuthor}
-              className="mb-4"
-            />
-          ))}
-        </section>
-      )}
-      <CommentForm
-        className="fixed bottom-0 z-100 w-full px-4 py-13px"
-        onComment={handleComment}
-      />
-    </main>
+    postDetail && (
+      <main className="relative pb-20 pt-5">
+        <PostItem post={postDetail.post} className="px-4" />
+        {postDetail.post.commentCount > 0 && (
+          <section className="border-t-1 border-gray-100 px-4 pt-5">
+            {commentList.comments.map(
+              ({ author: commentAuthor, ...comment }) => (
+                <CommentCard
+                  key={comment.id}
+                  comment={comment}
+                  user={commentAuthor}
+                  className="mb-4"
+                />
+              ),
+            )}
+          </section>
+        )}
+        <CommentForm
+          className="fixed bottom-0 z-100 w-full px-4 py-13px"
+          onComment={handleComment}
+        />
+      </main>
+    )
   );
 }
