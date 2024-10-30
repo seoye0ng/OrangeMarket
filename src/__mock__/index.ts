76819d99eab309b66c3fa907830c13afe73718b4
seoye0ng <bsy0313@gmail.com>
2024-10-30 18:43:09 +0900

    feat: MockAdapter로 api mocking하여 mockData가 잘 렌더링 되는지 테스트(Next-Study-habjung#30)

--- src/__mock__/index.ts
+++ src/__mock__/index.ts
@@ -8,4 +8,16 @@ import { postListData } from './postList'; // 모킹된 데이터
 const mock = new MockAdapter(instance);
 
 // '/post/feed'로 시작하는 모든 GET 요청을 모킹
-mock.onGet(/\/post\/feed.*/).reply(200, postListData);
+mock.onGet(/\/post\/feed.*/).reply((config) => {
+  // URL에서 쿼리 파라미터를 가져오기
+  const urlParams = new URLSearchParams(config.url?.split('?')[1]);
+
+  // 쿼리 파라미터에서 limit과 skip 값을 추출, 정수로 변환
+  const limit = parseInt(urlParams.get('limit') || '10', 10);
+  const skip = parseInt(urlParams.get('skip') || '0', 10);
+
+  // 전체 데이터에서 원하는 부분만 슬라이스
+  const posts = postListData.posts.slice(skip, skip + limit);
+
+  return [200, { posts }];
+});
