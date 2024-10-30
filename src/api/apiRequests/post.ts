05791dd8ea7abcbac35b336e2b0552c5cdeed9fb
seoye0ng <bsy0313@gmail.com>
2024-10-31 03:10:34 +0900

    feat: 댓글 리스트 api 요청 함수 작성(Next-Study-habjung#39)

--- src/api/apiRequests/post.ts
+++ src/api/apiRequests/post.ts
@@ -1,4 +1,5 @@
 import { getRequest } from '@/api/requests';
+import { ICommentList } from '@/api/types/comment';
 import { IPostList, IPostDetail } from '@/api/types/post';
 
 export const postList = async (limit: number, skip: number) => {
@@ -9,8 +10,14 @@ export const postList = async (limit: number, skip: number) => {
   return response;
 };
 
-export const postDetail = async (PostId: string) => {
-  const response = await getRequest<IPostDetail>(`/post/:${PostId}`);
+export const postDetail = async (postId: string) => {
+  const response = await getRequest<IPostDetail>(`/post/:${postId}`);
+
+  return response;
+};
+
+export const commentList = async (postId: string) => {
+  const response = await getRequest<ICommentList>(`/post/:${postId}/comments`);
 
   return response;
 };
