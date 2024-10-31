62427710684f82f52746af660937975710776844
seoye0ng <bsy0313@gmail.com>
2024-10-31 17:39:59 +0900

    feat: 댓글 리스트 api 요청 함수 작성(Next-Study-habjung#39)

--- src/api/apiRequests/post.ts
+++ src/api/apiRequests/post.ts
@@ -2,6 +2,7 @@ import { getRequest } from '@/api/requests';
 import { ICommentList } from '@/api/types/comment';
 import { IPostList, IPostDetail } from '@/api/types/post';
 
+/* --- 피드 리스트 GET 요청 --- */
 export const postList = async (limit: number, skip: number) => {
   const response = await getRequest<IPostList>(
     `/post/feed/?limit=${limit}&skip=${skip}`,
@@ -10,14 +11,16 @@ export const postList = async (limit: number, skip: number) => {
   return response;
 };
 
+/* --- 게시물 상세 GET 요청 --- */
 export const postDetail = async (postId: string) => {
-  const response = await getRequest<IPostDetail>(`/post/:${postId}`);
+  const response = await getRequest<IPostDetail>(`/post/${postId}`);
 
   return response;
 };
 
+/* --- 댓글 리스트 GET 요청 --- */
 export const commentList = async (postId: string) => {
-  const response = await getRequest<ICommentList>(`/post/:${postId}/comments`);
+  const response = await getRequest<ICommentList>(`/post/${postId}/comments`);
 
   return response;
 };
