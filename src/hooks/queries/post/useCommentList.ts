1cb646156dc5705415ab1a3841015c543e45a959
seoye0ng <bsy0313@gmail.com>
2024-10-31 20:25:45 +0900

    feat: 댓글 리스트 api 요청시 댓글 수가 1이상일 경우만 호출하도록 쿼리훅에 enabled 설정(Next-Study-habjung#39)

--- src/hooks/queries/post/useCommentList.ts
+++ src/hooks/queries/post/useCommentList.ts
@@ -1,11 +1,16 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useQuery } from '@tanstack/react-query';
 
 import { commentList } from '@/api/apiRequests/post';
 
-function useCommentList(postId: string) {
+function useCommentList(
+  postId: string,
+  options?: { enabled?: boolean; initialData?: any },
+) {
   return useQuery({
     queryKey: ['commentsList', postId],
     queryFn: () => commentList(postId),
+    ...options, // options를 spread하여 추가 설정을 적용
   });
 }
 
