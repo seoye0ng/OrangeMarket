2201b19e469d13a364fcaa6e8fd6c5b1fe68ef43
seoye0ng <bsy0313@gmail.com>
2024-10-31 20:00:02 +0900

    feat: 댓글 작성 쿼리훅 호출 및 댓글 리스트 api 요청시 댓글 수가 1이상일 경우만 호출 구현(Next-Study-habjung#39) mutate의 return 값을 가져올 수 있도록 mutateAsync메서드 사용, 댓글 추가시마다 댓글리스트 api 반복 요청 방지

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -1,11 +1,14 @@
 'use client';
 
 import '@/__mock__/';
+import { IComment, ICommentRequest } from '@/api/types/comment';
 import CommentForm from '@/components/common/form/comment-form/CommentForm';
 import CommentCard from '@/components/common/post-item/comment-card/CommentCard';
 import PostItem from '@/components/common/post-item/PostItem';
 import useCommentList from '@/hooks/queries/post/useCommentList';
+import useCreateComment from '@/hooks/queries/post/useCreateComment';
 import usePostDetail from '@/hooks/queries/post/usePostDetail';
+import { useEffect, useState } from 'react';
 
 interface PostDetailPageProps {
   params: { id: string };
@@ -13,40 +16,55 @@ interface PostDetailPageProps {
 
 export default function PostDetailsPage({ params }: PostDetailPageProps) {
   const { id } = params; // URL의 id 파라미터 추출
+  const [commentList, setCommentList] = useState<IComment[]>([]);
 
   const { data: postDetail, isLoading, error } = usePostDetail(id);
-  const { data: commentList } = useCommentList(id);
-  console.log('commentList', commentList);
+  const { mutateAsync: createComment } = useCreateComment();
+  const commentCount = postDetail?.post.commentCount ?? 0; // 기본값을 0으로 설정
+  const { data: comments } = useCommentList(id, {
+    enabled: commentCount > 0, // commentCount가 1 이상일 때만 요청
+    initialData: [], // 기본값을 빈 배열로 설정
+  });
+
+  useEffect(() => {
+    if (comments) {
+      setCommentList(comments.comments);
+    }
+  }, [comments]);
+
   if (isLoading) return <p>Loading...</p>;
   if (!postDetail || error) return <p>Error: {error?.message}</p>;
 
-  const handleComment = (commentTerm: string) => {
+  const handleAddComment = async (commentTerm: string) => {
     console.log('input ref', commentTerm);
-    // 새 댓글이 추가되면 CommentCard가 다시 렌더링이 되어야 함.
-    // TODO: 이때 CommentList를 다시 불러와야하고 이렇게 되면 기존의 CommentCard들이 싹 리렌더링이 됨 -> CommentCard를 React.memo로 감싸서 불필요한 리렌더링을 방지
+
+    const data: ICommentRequest = { comment: { content: commentTerm } };
+    try {
+      const response = await createComment({ postId: id, data });
+      setCommentList((prevComments) => [...prevComments, response.comments]); // 댓글 목록 업데이트
+    } catch (error) {
+      console.error('댓글 작성 오류:', error);
+    }
   };
 
   return (
     <main className="relative pb-20 pt-5">
       <PostItem post={postDetail.post} className="px-4" />
-      {postDetail.post.commentCount > 0 && (
+      {commentList && (
         <section className="border-t-1 border-gray-100 px-4 pt-5">
-          {/* commentList.comments */}
-          {commentList?.comments.map(
-            ({ author: commentAuthor, ...comment }) => (
-              <CommentCard
-                key={comment.id}
-                comment={comment}
-                user={commentAuthor}
-                className="mb-4"
-              />
-            ),
-          )}
+          {commentList.map(({ author: commentAuthor, ...comment }) => (
+            <CommentCard
+              key={comment.id}
+              comment={comment}
+              user={commentAuthor}
+              className="mb-4"
+            />
+          ))}
         </section>
       )}
       <CommentForm
         className="fixed bottom-0 z-100 w-full px-4 py-13px"
-        onComment={handleComment}
+        onSubmit={handleAddComment}
       />
     </main>
   );
