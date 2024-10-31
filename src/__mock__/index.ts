668bf7afed920f3c6a79f7b398a57dfae1242e65
seoye0ng <bsy0313@gmail.com>
2024-10-31 17:41:57 +0900

    feat: 댓글 리스트 api 요청 모킹 테스트 함수 구현(Next-Study-habjung#39)

--- src/__mock__/index.ts
+++ src/__mock__/index.ts
@@ -1,14 +1,14 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import MockAdapter from 'axios-mock-adapter';
 
 import instance from '@/api/index'; // axios 인스턴스 가져오기
 
-import { postListData } from './postList'; // 모킹된 데이터
+import { commentList } from './responseData/commentList';
+import { postListData } from './responseData/postList'; // 모킹된 데이터
 
 // Mock Adapter 인스턴스 생성
 const mock = new MockAdapter(instance);
 
-// '/post/feed'로 시작하는 모든 GET 요청을 모킹
+/* --- '/post/feed'로 시작하는 모든 GET 요청을 모킹 --- */
 mock.onGet(/\/post\/feed.*/).reply((config) => {
   // URL에서 쿼리 파라미터를 가져오기
   const urlParams = new URLSearchParams(config.url?.split('?')[1]);
@@ -23,7 +23,9 @@ mock.onGet(/\/post\/feed.*/).reply((config) => {
   return [200, { posts }];
 });
 
-mock.onGet(/\/post\/:\d+/).reply((config) => {
+/* --- 게시물 상세 GET 요청 모킹 --- */
+
+mock.onGet(/\/post\/\d+$/).reply((config) => {
   const { url } = config;
 
   // url이 없을 경우 에러 처리
@@ -32,10 +34,7 @@ mock.onGet(/\/post\/:\d+/).reply((config) => {
   }
 
   const urlParts = url.split('/');
-
-  // 마지막 부분에서 ':'가 포함된 숫자 가져오기
-  const lastPart = urlParts[urlParts.length - 1]; // ':1'과 같은 형식
-  const postId = lastPart.substring(1); // ':'를 제거하고 숫자만 가져옴
+  const postId = urlParts[urlParts.length - 1];
 
   // mock 데이터에서 postId에 해당하는 포스트 찾기
   const post = postListData.posts.find((p) => p.id === postId);
@@ -44,4 +43,21 @@ mock.onGet(/\/post\/:\d+/).reply((config) => {
     return [200, { post }]; // 포스트가 있는 경우
   }
   return [404, { message: '존재하지 않는 게시글입니다.' }]; // 포스트가 없는 경우
+});
+
+/* --- 댓글 GET 요청 모킹 --- */
+mock.onGet(/\/post\/\d+\/comments/).reply((config) => {
+  const { url } = config;
+
+  if (!url) {
+    return [400, { message: '잘못된 요청입니다.' }];
+  }
+
+  const urlParts = url.split('/');
+  const postId = urlParts[urlParts.length - 2]; // 'comments'의 이전 부분
+
+  // 모의 데이터에서 postId에 해당하는 댓글 찾기
+  const comments = commentList.comments.filter((c) => c.id === postId);
+
+  return [200, { comments }];
 });
