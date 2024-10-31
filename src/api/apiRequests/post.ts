333400be221e83619e43ef4c8c4d3bda318bd66d
seoye0ng <bsy0313@gmail.com>
2024-10-31 19:52:38 +0900

    feat: 댓글 작성 api 요청 함수 구현(Next-Study-habjung#39)

--- src/api/apiRequests/post.ts
+++ src/api/apiRequests/post.ts
@@ -1,5 +1,9 @@
-import { getRequest } from '@/api/requests';
-import { ICommentList } from '@/api/types/comment';
+import { getRequest, postRequest } from '@/api/requests';
+import {
+  ICommentList,
+  ICommentRequest,
+  ICommentResponse,
+} from '@/api/types/comment';
 import { IPostList, IPostDetail } from '@/api/types/post';
 
 /* --- 피드 리스트 GET 요청 --- */
@@ -21,6 +25,17 @@ export const postDetail = async (postId: string) => {
 /* --- 댓글 리스트 GET 요청 --- */
 export const commentList = async (postId: string) => {
   const response = await getRequest<ICommentList>(`/post/${postId}/comments`);
+
+  return response;
+};
+
+/* --- 댓글 POST 요청 --- */
+export const createComment = async (postId: string, data: ICommentRequest) => {
+  console.log(postId, data);
+  const response = await postRequest<ICommentResponse, ICommentRequest>(
+    `/post/${postId}/comments`,
+    data,
+  );
 
   return response;
 };
