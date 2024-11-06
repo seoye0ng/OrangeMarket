d18e1ea3a7339d2173fcc62d9c942f9755811b5c
seoye0ng <bsy0313@gmail.com>
2024-11-06 14:04:42 +0900

    feat: not-found 페이지 ui 구현(Next-Study-habjung#38)

---
+++ src/app/not-found.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+
+import CustomButton from '@/components/common/button/Button';
+
+export default function NotFound() {
+  return (
+    <main className="h-screen pt-52 text-center">
+      <Image
+        src="/assets/icons/icon-404.svg"
+        alt="404 아이콘 이미지"
+        width={158}
+        height={158}
+        className="mx-auto"
+      />
+      <p className="mb-5 mt-7 text-14px font-normal text-gray-300">
+        페이지를 찾을 수 없습니다. 🥲
+      </p>
+      <CustomButton type="button" className="max-w-32" color="primary">
+        이전 페이지
+      </CustomButton>
+    </main>
+  );
+}
