52aede33c78729df4f5a83c6f87b88558beff16f
seoye0ng <bsy0313@gmail.com>
2024-11-10 18:23:24 +0900

    refactor: Profile 페이지 컨텐츠를 app 폴더에서 pages 폴더로 이동(Next-Study-habjung#45)

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,34 +1,5 @@
-'use client';
-
-import { postListData } from '@/__mock__/responseData/postList';
-import { productList } from '@/__mock__/responseData/productList';
-import PostSection from '@/components/profile/post/PostSection';
-import ProductListSection from '@/components/profile/product/ProductListSection';
-import UserInfoSection from '@/components/profile/user-info/UserInfoSection';
-
-const userProfile = {
-  _id: '',
-  accountname: '@alpaca__!',
-  image: '',
-  username: '오렌지 알파카',
-  intro: '안녕 난 오렌지 알파카!',
-  follower: [''],
-  following: [''],
-  followerCount: 102345,
-  followingCount: 50,
-  isfollow: false,
-};
+import ProfilePage from '@/components/pages/ProfilePage';
 
 export default function Profile() {
-  // const type = 'user' || 'otherUser'
-  return (
-    <main className="flex flex-col gap-6px bg-gray-50">
-      <UserInfoSection
-        userProfile={userProfile}
-        className="bg-white pb-26px pt-30px"
-      />
-      <ProductListSection className="bg-white py-5" productList={productList} />
-      <PostSection className="bg-white" postList={postListData} />
-    </main>
-  );
+  return <ProfilePage />;
 }
