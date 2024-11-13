1beeb4725ca4922fb2fb0f0196500746b3eb7a49
seoye0ng <bsy0313@gmail.com>
2024-11-13 22:10:31 +0900

    rename: (private) 경로그룹 생성(파일 이동)(Next-Study-habjung#46)

--- src/app/(private)/feed/page.tsx
+++ src/app/(private)/feed/page.tsx
@@ -1,5 +1,7 @@
 import PostFeedPage from '@/components/pages/PostFeedPage';
 
 export default function Home() {
+  // 로그인x 시에는 splash 이미지
+  // 로그인을 했을 때는 PostFeedPage를 렌더링
   return <PostFeedPage />;
 }
