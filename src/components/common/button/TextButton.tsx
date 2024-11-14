e98e18a6fed632df2f03400b81e57dfd5c527139
seoye0ng <bsy0313@gmail.com>
2024-11-15 00:38:55 +0900

    feat: TextButton 컴포넌트 제작(Next-Study-habjung#49)

---
+++ src/components/common/button/TextButton.tsx
@@ -0,0 +1,35 @@
+import { Button } from '@nextui-org/react';
+
+import Text from '@/components/common/text/Text';
+import { FontSize, Color } from '@/styles/token';
+
+interface ITextButton {
+  children: React.ReactNode;
+  onClick?: () => void;
+  className?: string;
+  color?: Color;
+  size: FontSize;
+  weight?: 'thin' | 'light' | 'normal' | 'medium' | 'bold';
+}
+
+export default function TextButton({
+  children,
+  onClick,
+  className,
+  color = 'primary-100',
+  size = '12px',
+  weight = 'normal',
+}: ITextButton) {
+  return (
+    <Button
+      className={className}
+      variant="light"
+      radius="full"
+      onClick={onClick}
+    >
+      <Text size={size} color={color} weight={weight}>
+        {children}
+      </Text>
+    </Button>
+  );
+}
