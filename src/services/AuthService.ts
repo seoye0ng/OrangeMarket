a5a5fa5e19e62251e28a32465795b093c4f537cc
seoye0ng <bsy0313@gmail.com>
2024-11-18 22:50:49 +0900

    feat: 사용자 상태와 인증 등의 비즈니스 로직을 담당하는 AuthService 클래스 구현(Next-Study-habjung#50)

---
+++ src/services/AuthService.ts
@@ -0,0 +1,38 @@
+import StorageService from './StorageService';
+
+class AuthService {
+  private static userAccount: string | null = StorageService.get('userAccount');
+  private static token: string | null = StorageService.get('token');
+
+  static login(userAccount: string, token: string): void {
+    this.userAccount = userAccount;
+    this.token = token;
+    StorageService.setStorageAdapter('localStorage');
+    StorageService.set('userAccount', userAccount);
+    StorageService.setStorageAdapter('cookie');
+    StorageService.set('token', token);
+  }
+
+  static logout(): void {
+    this.userAccount = null;
+    this.token = null;
+    StorageService.setStorageAdapter('localStorage');
+    StorageService.remove('userAccount');
+    StorageService.setStorageAdapter('cookie');
+    StorageService.remove('token');
+  }
+
+  static getUser(): string | null {
+    return this.userAccount || StorageService.get('userAccount');
+  }
+
+  static getToken(): string | null {
+    return this.token || StorageService.get('token');
+  }
+
+  static isAuthenticated(): boolean {
+    return !!this.userAccount && !!this.token;
+  }
+}
+
+export default AuthService;
