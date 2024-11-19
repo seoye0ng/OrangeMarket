7ae237f253e4a0a00f464ae1174fb58c7fde3d09
seoye0ng <bsy0313@gmail.com>
2024-11-19 21:49:52 +0900

    feat: isHeaderClick가 true라면 onSubmit이 실행되도록 작성(Next-Study-habjung#40)

--- src/components/common/form/upload-form/UploadForm.tsx
+++ src/components/common/form/upload-form/UploadForm.tsx
@@ -2,16 +2,12 @@ import { useFormContext } from 'react-hook-form';
 
 import { IUploadPostRequest } from '@/api/types/post';
 
-interface UploadFormProps {
-  onSubmit: (data: IUploadPostRequest) => void;
-}
-
-export default function UploadForm({ onSubmit }: UploadFormProps) {
+export default function UploadForm() {
   // useFormContext의 타입 지정
-  const { register, handleSubmit } = useFormContext<IUploadPostRequest>();
+  const { register } = useFormContext<IUploadPostRequest>();
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form>
       <input
         {...register('post.content', { required: '게시글을 입력해주세요.' })}
         placeholder="게시글 입력하기"
--- src/components/pages/UploadPage.tsx
+++ src/components/pages/UploadPage.tsx
@@ -1,11 +1,14 @@
 'use client';
 
+import { useEffect } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
 
 import { IUploadPostRequest } from '@/api/types/post';
 import UploadForm from '@/components/common/form/upload-form/UploadForm';
+import { useHeaderContext } from '@/context/provider/headerContext';
 
 export default function UploadPage() {
+  const { isHeaderClick, setIsHeaderClick } = useHeaderContext();
   const methods = useForm({
     defaultValues: {
       post: {
@@ -16,12 +19,22 @@ export default function UploadPage() {
   });
 
   const onSubmit = (data: IUploadPostRequest) => {
+    console.log(isHeaderClick);
     console.log(data); // 제출된 폼 데이터 출력
   };
 
+  useEffect(() => {
+    if (isHeaderClick) {
+      methods.handleSubmit(onSubmit)();
+    }
+    return () => {
+      setIsHeaderClick(false);
+    };
+  }, [isHeaderClick, methods, onSubmit, setIsHeaderClick]);
+
   return (
     <FormProvider {...methods}>
-      <UploadForm onSubmit={onSubmit} />
+      <UploadForm />
     </FormProvider>
   );
 }
