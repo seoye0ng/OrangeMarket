7e466bd782d6c99c6abe4448d903b287c309d680
seoye0ng <bsy0313@gmail.com>
2024-11-19 21:51:26 +0900

    feat: useUploadPost 쿼리훅 적용(Next-Study-habjung#40)

--- src/components/pages/UploadPage.tsx
+++ src/components/pages/UploadPage.tsx
@@ -6,9 +6,11 @@ import { FormProvider, useForm } from 'react-hook-form';
 import { IUploadPostRequest } from '@/api/types/post';
 import UploadForm from '@/components/common/form/upload-form/UploadForm';
 import { useHeaderContext } from '@/context/provider/headerContext';
+import useUploadPost from '@/hooks/queries/upload/useUploadPost';
 
 export default function UploadPage() {
   const { isHeaderClick, setIsHeaderClick } = useHeaderContext();
+  const { mutate: uploadPost } = useUploadPost();
   const methods = useForm({
     defaultValues: {
       post: {
@@ -21,6 +23,7 @@ export default function UploadPage() {
   const onSubmit = (data: IUploadPostRequest) => {
     console.log(isHeaderClick);
     console.log(data); // 제출된 폼 데이터 출력
+    uploadPost(data);
   };
 
   useEffect(() => {
