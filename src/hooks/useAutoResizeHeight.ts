e5dd3fdaaca6cf894a3610f134e09f8f98596b8f
seoye0ng <bsy0313@gmail.com>
2024-11-23 00:02:11 +0900

    feat: useMemo로 throttle을 캐싱하면서, useEffect로 cleanup을 처리(Next-Study-habjung#51)

--- src/hooks/useAutoResizeHeight.ts
+++ src/hooks/useAutoResizeHeight.ts
@@ -1,15 +1,30 @@
-import { useRef, useCallback } from 'react';
+import { throttle } from 'lodash';
+import { useRef, useCallback, useMemo, useEffect } from 'react';
 
-export function useAutoResizeHeight<T extends HTMLElement>() {
-  const elementRef = useRef<T>(null);
+export function useAutoResizeHeight<T extends HTMLElement>(throttleTime = 100) {
+  const elementRef = useRef<T | null>(null);
 
   const resizeToFitContent = useCallback(() => {
     const element = elementRef.current;
     if (element) {
       element.style.height = 'auto'; // 높이 초기화
-      element.style.height = `${element.scrollHeight}px`; // 콘텐츠에 따라 높이 조정
+      element.style.height = `${element.scrollHeight}px`; // 콘텐츠에  조정
     }
   }, []);
 
-  return { elementRef, resizeToFitContent };
+  // Throttled function 생성
+  const throttledResize = useMemo(
+    () => throttle(resizeToFitContent, throttleTime),
+    [resizeToFitContent, throttleTime],
+  );
+
+  // Cleanup: Throttle 함수 정리
+  useEffect(() => {
+    return () => {
+      console.log('cl');
+      throttledResize.cancel(); // 메모리 누수 방지
+    };
+  }, [throttledResize]);
+
+  return { elementRef, throttledResize };
 }
