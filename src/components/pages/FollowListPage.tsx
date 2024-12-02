686946ed573aac1ff6a88d9197d988e5a2353e71
seoye0ng <bsy0313@gmail.com>
2024-12-02 19:12:29 +0900

    feat: 팔로워/팔로잉 목록 쿼리훅 적용 및 무한스크롤 구현(Next-Study-habjung#54)

--- src/components/pages/FollowListPage.tsx
+++ src/components/pages/FollowListPage.tsx
@@ -1,33 +1,57 @@
+'use client';
+
+import '@/__mock__';
+
+import { Spinner } from '@nextui-org/react';
+import { usePathname } from 'next/navigation';
+import { useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
+
 import { CustomButton } from '@/components/common/button';
 import UserCard from '@/components/common/post-item/user-card/UserCard';
+import useInfiniteFollowList from '@/hooks/queries/follow/useInfiniteFollowList';
 
-const userProfile = {
-  _id: '',
-  accountname: 'alpaca__!',
-  image: '',
-  username: '오렌지 알파카',
-  intro: '안녕 난 오렌지 알파카!',
-  follower: [''],
-  following: [''],
-  followerCount: 102345,
-  followingCount: 50,
-  isfollow: true,
-};
+const FOLLOW_LIST_LIMIT = 20;
+const getFollowListType = (
+  rawFollowListType: string,
+): 'follower' | 'following' =>
+  rawFollowListType === 'follower' || rawFollowListType === 'following'
+    ? rawFollowListType
+    : 'following';
 
-const users = [userProfile, userProfile, userProfile];
+export default function FollowListPage() {
+  const pathname = usePathname();
+  const [accountName, rawFollowListType] = pathname
+    .replace(/^\//, '')
+    .split('/');
+  const followListType = getFollowListType(rawFollowListType);
 
-interface IFollowListPageProps {
-  accountName: string;
-}
+  const {
+    data: users,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useInfiniteFollowList({
+    accountName,
+    followListType,
+    limit: FOLLOW_LIST_LIMIT,
+  });
 
-export default function FollowListPage({ accountName }: IFollowListPageProps) {
-  console.log(accountName);
+  const { ref, inView } = useInView();
+
+  // 요소가 뷰포트에 들어오면 다음 페이지 가져오기
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, fetchNextPage]);
+
   return (
     <main className="px-4 pt-6">
       <ul className="flex flex-col gap-4">
-        {users.map((user) => {
-          return (
-            <UserCard imageSize="50px" userProfile={user}>
+        {users?.pages.map((page) =>
+          page.map((user) => (
+            <UserCard imageSize="50px" userProfile={user} key="user._id">
               <CustomButton
                 color={user.isfollow ? 'white' : 'primary'}
                 size="s"
@@ -35,9 +59,19 @@ export default function FollowListPage({ accountName }: IFollowListPageProps) {
                 {user.isfollow ? '취소' : '팔로우'}
               </CustomButton>
             </UserCard>
-          );
-        })}
+          )),
+        )}
       </ul>
+      {/* 스크롤 로딩 처리 */}
+      {hasNextPage && (
+        <div ref={ref} className="pb-14">
+          {isFetchingNextPage ? (
+            <Spinner color="primary" size="lg" />
+          ) : (
+            '스크롤 내리기'
+          )}
+        </div>
+      )}
     </main>
   );
 }
