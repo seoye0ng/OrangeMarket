0afeaaf4ab233b25cca835d33c94dca5ee872e6f
seoye0ng <bsy0313@gmail.com>
2024-12-08 14:42:58 +0900

    refactor: SocialActionButton 컴포넌트로 UI 분리(Next-Study-habjung#56)

---
+++ src/components/profile/user-info/SocialActionButton.tsx
@@ -0,0 +1,36 @@
+import Image from 'next/image';
+
+import IconButton from '@/components/common/button/IconButton';
+import FollowButton from '@/components/follow/FollowButton';
+import classNames from 'classnames';
+
+interface ISocialActionButtons {
+  type: '채팅' | '공유';
+  onClick?: () => void;
+}
+
+const socialActionButtonMaps: { [key: string]: string } = {
+  채팅: '/assets/icons/icon-message-circle.svg',
+  공유: '/assets/icons/icon-share.svg',
+};
+
+export default function SocialActionButton({
+  onClick,
+  type,
+}: ISocialActionButtons) {
+  const buttonImage = socialActionButtonMaps[type];
+  return (
+    <IconButton
+      label={`${type} 버튼 아이콘`}
+      className="rounded-full border-1 border-gray-100 p-6px"
+      onClick={onClick}
+    >
+      <Image
+        src={buttonImage}
+        alt={`${type} 버튼 아이콘 이미지`}
+        width={20}
+        height={20}
+      />
+    </IconButton>
+  );
+}
