216bf2b5e7a578d6f2681fd6f941bdb0065c7e8d
seoye0ng <bsy0313@gmail.com>
2024-12-10 03:04:39 +0900

    feat: userPostList API 요청 쿼리훅 적용(Next-Study-habjung#33) react-intersection-observer를 사용한 무한스크롤 구현

--- src/components/profile/container/PostContainer.tsx
+++ src/components/profile/container/PostContainer.tsx
@@ -1,32 +1,64 @@
+/* eslint-disable react/no-array-index-key */
+
 'use client';
 
+import { Spinner } from '@nextui-org/react';
 import classNames from 'classnames';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useInView } from 'react-intersection-observer';
 
-import { IPostList } from '@/api/types/post';
+import useInfiniteUserPostList from '@/hooks/queries/post/useInfiniteUserPostList';
 
 import { IconSwitcher } from '../components/post/IconSwitcher';
 import { PostAlbumView, PostListView } from '../components/post/PostView';
 
 interface IPostSection {
+  accountName: string;
   className?: string;
-  postList: IPostList;
 }
+const POSTS_LIMIT = 12;
 
-export default function PostContainer({ className, postList }: IPostSection) {
+export default function PostContainer({
+  className,
+  accountName,
+}: IPostSection) {
+  const { data, fetchNextPage, hasNextPage, isFetchingNextPage } =
+    useInfiniteUserPostList(accountName, POSTS_LIMIT);
+  const { ref, inView } = useInView();
+
   const [postType, setPostType] = useState<'list' | 'album'>('list');
+
+  useEffect(() => {
+    if (inView && hasNextPage) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, fetchNextPage]);
 
   const handlePostType = (type: 'list' | 'album') => {
     if (type === postType) return;
-    console.log('change일 때만');
     setPostType(type);
   };
 
   return (
     <section className={classNames('', className)}>
       <IconSwitcher postType={postType} onSwitch={handlePostType} />
-      {postType === 'list' && <PostListView postList={postList} />}
-      {postType === 'album' && <PostAlbumView postList={postList} />}
+      <div className="px-4 pb-20 pt-4">
+        {postType === 'list' &&
+          data?.pages.map((page) => <PostListView postList={page} />)}
+        {postType === 'album' &&
+          data?.pages.map((page, i) => (
+            <PostAlbumView key={i} postList={page} />
+          ))}
+      </div>
+      {hasNextPage && (
+        <div ref={ref} className="pb-14">
+          {isFetchingNextPage ? (
+            <Spinner color="primary" size="lg" />
+          ) : (
+            '스크롤 내리기'
+          )}
+        </div>
+      )}
     </section>
   );
 }
