e71e95c5c69fd5e589dda85ffe5d317f047ebebd
seoye0ng <bsy0313@gmail.com>
2024-12-11 23:13:41 +0900

    feat: Search API 요청 쿼리훅 적용(Next-Study-habjung#61) useSearch에서 자동으로 실행되지 않도록 enabled: false 옵션을 추가하고, refetch 메서드를 사용해 검색시에만 실행하도록 변경

--- src/components/pages/SearchPage.tsx
+++ src/components/pages/SearchPage.tsx
@@ -4,30 +4,42 @@
 
 import { useEffect } from 'react';
 
-import { userList } from '@/__mock__/responseData/userList';
+import '@/__mock__';
+
 import { IUserProfileBase } from '@/api/types/user';
 import UserCard from '@/components/common/post-item/user-card/UserCard';
 import { useHeaderContext } from '@/context/provider/HeaderContext';
 import { useSearchContext } from '@/context/provider/SearchContext';
+import useSearch from '@/hooks/queries/search/useSearch';
+
+import EmptyState from '../profile/components/empty-state/EmptyState';
 
 export default function SearchPage() {
   const { isHeaderClick, setIsHeaderClick } = useHeaderContext();
-  const { searchTerm, setSearchTerm } = useSearchContext();
+  const { searchTerm } = useSearchContext();
+
+  const {
+    data: userList,
+    refetch,
+    isError,
+  } = useSearch(searchTerm, { enabled: false });
 
   useEffect(() => {
     if (isHeaderClick && searchTerm) {
-      // search(searchTerm);
-      console.log('searchTerm', searchTerm);
+      refetch();
     }
     return () => {
       setIsHeaderClick(false);
-      setSearchTerm('');
     };
   }, [isHeaderClick]);
 
+  if (isError) {
+    return <EmptyState className="mx-4" message="유저가 없습니다." />;
+  }
+
   return (
     <main className="mt-5 px-4">
-      {userList.map((user: IUserProfileBase) => (
+      {userList?.map((user: IUserProfileBase) => (
         <UserCard
           key={user.accountname}
           className="mb-5"
