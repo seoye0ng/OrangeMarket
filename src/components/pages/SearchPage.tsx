c858fb1b9ec299b23a1659a1a60d65887ccbbf6d
seoye0ng <bsy0313@gmail.com>
2024-12-11 22:34:49 +0900

    feat: Header에서 검색시에 SearchPage는 searchTerm을 구독하여 값이 업데이트(Next-Study-habjung#61)

--- src/components/pages/SearchPage.tsx
+++ src/components/pages/SearchPage.tsx
@@ -1,15 +1,35 @@
-/* eslint-disable no-underscore-dangle */
+/* eslint-disable react-hooks/exhaustive-deps */
+
+'use client';
+
+import { useEffect } from 'react';
 
 import { userList } from '@/__mock__/responseData/userList';
 import { IUserProfileBase } from '@/api/types/user';
 import UserCard from '@/components/common/post-item/user-card/UserCard';
+import { useHeaderContext } from '@/context/provider/HeaderContext';
+import { useSearchContext } from '@/context/provider/SearchContext';
 
 export default function SearchPage() {
+  const { isHeaderClick, setIsHeaderClick } = useHeaderContext();
+  const { searchTerm, setSearchTerm } = useSearchContext();
+
+  useEffect(() => {
+    if (isHeaderClick && searchTerm) {
+      // search(searchTerm);
+      console.log('searchTerm', searchTerm);
+    }
+    return () => {
+      setIsHeaderClick(false);
+      setSearchTerm('');
+    };
+  }, [isHeaderClick]);
+
   return (
     <main className="mt-5 px-4">
       {userList.map((user: IUserProfileBase) => (
         <UserCard
-          key={user._id}
+          key={user.accountname}
           className="mb-5"
           imageSize="42px"
           userProfile={user}
