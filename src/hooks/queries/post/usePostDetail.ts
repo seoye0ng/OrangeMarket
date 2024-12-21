1628337227cc5576593583436f81aea77fa2b124
seoye0ng <bsy0313@gmail.com>
2024-12-21 16:23:15 +0900

    feat: postKeys를 제작해 post 관련 쿼리키 관리(Next-Study-habjung#42)

--- src/hooks/queries/post/usePostDetail.ts
+++ src/hooks/queries/post/usePostDetail.ts
@@ -1,10 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
 
 import { postDetail } from '@/api/apiRequests/post';
+import { postKeys } from '@/queries/post/postKeys';
 
 function usePostDetail(postId: string) {
   return useQuery({
-    queryKey: ['postDetail', postId],
+    queryKey: postKeys.detail(postId),
     queryFn: () => postDetail(postId),
   });
 }
