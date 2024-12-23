737b1aa46e583a1bf7dc4e19efc869ee8af54382
seoye0ng <bsy0313@gmail.com>
2024-12-23 23:30:48 +0900

    feat: commentKeys를 제작해 comment 관련 쿼리키 관리(Next-Study-habjung#42)

--- src/hooks/queries/comment/useCommentList.ts
+++ src/hooks/queries/comment/useCommentList.ts
@@ -2,13 +2,14 @@
 import { useQuery } from '@tanstack/react-query';
 
 import { commentList } from '@/api/apiRequests/comment';
+import { commentKeys } from '@/queries/comment/commentKeys';
 
 function useCommentList(
   postId: string,
   options?: { enabled?: boolean; initialData?: any },
 ) {
   return useQuery({
-    queryKey: ['commentsList', postId],
+    queryKey: commentKeys.list(postId),
     queryFn: () => commentList(postId),
     ...options, // options를 spread하여 추가 설정을 적용
   });
