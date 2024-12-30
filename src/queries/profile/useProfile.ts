8785a9081e18473062a2c30ec03614ec668eaf70
seoye0ng <bsy0313@gmail.com>
2024-12-31 00:32:59 +0900

    feat: 프로필 쿼리훅에 option 값을 받을 수 있도록 추가(Next-Study-habjung#57)

--- src/queries/profile/useProfile.ts
+++ src/queries/profile/useProfile.ts
@@ -3,10 +3,11 @@ import { useQuery } from '@tanstack/react-query';
 import { profile } from '@/api/apiRequests/profile';
 import { profileKeys } from '@/queries/profile/profileKeys';
 
-function useProfile(accountName: string) {
+function useProfile(accountName: string, options = {}) {
   return useQuery({
     queryKey: profileKeys.detail(accountName),
     queryFn: () => profile(accountName),
+    ...options,
   });
 }
 
