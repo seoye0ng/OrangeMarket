dee9137fa83812416e96623cf086e25fa96ac243
seoye0ng <bsy0313@gmail.com>
2025-01-03 15:07:02 +0900

    refactor: 프로필 수정 쿼리훅 수정(Next-Study-habjung#58) 성공시 코드 삭제

--- src/queries/profile/useEditAccount.ts
+++ src/queries/profile/useEditAccount.ts
@@ -3,23 +3,14 @@ import { AxiosError } from 'axios';
 
 import { editAccount } from '@/api/apiRequests/profile';
 import { IEditAccountRequest, IProfileResponse } from '@/api/types/user';
-import useNavigate from '@/hooks/useNavigate';
 
 function useEditAccount() {
-  const { goBack } = useNavigate();
-
   const onError = (error: AxiosError) => {
     console.error(error);
   };
 
-  const onSuccess = () => {
-    console.log('success');
-    goBack();
-  };
-
   return useMutation<IProfileResponse, AxiosError, IEditAccountRequest>({
     mutationFn: editAccount,
-    onSuccess,
     onError,
   });
 }
