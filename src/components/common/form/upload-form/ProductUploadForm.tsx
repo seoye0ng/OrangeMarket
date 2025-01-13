b09166a4307e4b529c69e14c037268d04b6107fe
seoye0ng <bsy0313@gmail.com>
2025-01-13 22:18:16 +0900

    feat: 이미지 선택시 폼 데이터에 저장 및 미리보기 기능 추가(Next-Study-habjung#26)

--- src/components/common/form/upload-form/ProductUploadForm.tsx
+++ src/components/common/form/upload-form/ProductUploadForm.tsx
@@ -1,3 +1,5 @@
+import { useFormContext } from 'react-hook-form';
+
 import ImageInput from '@/components/common/input/ImageInput';
 import Text from '@/components/common/text/Text';
 import { productFields } from '@/config/uploadFieldConfig';
@@ -6,20 +8,43 @@ import { usePreviewImage } from '@/hooks/usePreviewImage';
 import FieldsLayout from '../auth-form/FieldsLayout';
 import PreviewImage from '../preview-image/PreviewImage';
 
-export default function ProductUploadForm() {
-  const { preview } = usePreviewImage();
+interface IProductUploadFormProps {
+  className?: string;
+}
+
+export default function ProductUploadForm({
+  className,
+}: IProductUploadFormProps) {
+  const { setValue } = useFormContext();
+
+  const { preview, addImage, deleteImage } = usePreviewImage();
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const imageFile = e.target.files?.[0];
+    if (imageFile) {
+      setValue('product.image', imageFile.name);
+      addImage(imageFile);
+    }
+  };
 
   return (
-    <form>
-      <FieldsLayout fields={productFields}>
+    <form className={className}>
+      <FieldsLayout fields={productFields} prefix="product">
         <Text size="12px" color="gray-300" className="mr-auto">
           이미지 등록
         </Text>
         <div className="relative h-[204px] w-full rounded-10px bg-gray-100">
-          {preview && <PreviewImage previewUrl={preview} className="h-full" />}
+          {preview && (
+            <PreviewImage
+              previewUrl={preview}
+              onClick={deleteImage}
+              className="h-full"
+            />
+          )}
           <ImageInput
             inputSize="s"
             className="absolute bottom-3 right-3 z-10"
+            onChange={handleImageChange}
           />
         </div>
       </FieldsLayout>
