1d255de8deb4cafb0f1f56be6b4854992d543e95
seoye0ng <bsy0313@gmail.com>
2025-01-17 02:17:20 +0900

    refactor: ImageInput forwardRef 제거(Next-Study-habjung#69) ref를 사용하지 않아도 되는 상황에서는 성능이나 유지보수 측면에서 제거하는게 적합하다고 판단

--- src/components/common/input/ImageInput.tsx
+++ src/components/common/input/ImageInput.tsx
@@ -1,6 +1,5 @@
 import classNames from 'classnames';
 import Image from 'next/image';
-import { ForwardedRef, forwardRef } from 'react';
 
 type InputSize = 's' | 'm';
 type InputColor = 'primary-100' | 'gray-200';
@@ -30,53 +29,43 @@ const inputStyles: {
   },
 };
 
-const ImageInput = forwardRef(
-  (
-    {
-      inputSize = 'm',
-      color = 'primary-100',
-      id = 'imageFile',
-      accept = 'image/jpg, image/jpeg, image/png, image/gif',
-      className,
-      onChange,
-      ...inputProps
-    }: ImageInputProps,
-    ref: ForwardedRef<HTMLInputElement>,
-  ) => {
-    const size = inputStyles.sizes[inputSize];
+export default function ImageInput({
+  inputSize = 'm',
+  color = 'primary-100',
+  id = 'imageFile',
+  accept = 'image/jpg, image/jpeg, image/png, image/gif',
+  className,
+  ...inputProps
+}: ImageInputProps) {
+  const size = inputStyles.sizes[inputSize];
 
-    return (
-      <>
-        <label
-          htmlFor={id}
-          className={classNames(
-            'relative cursor-pointer rounded-full',
-            size.label,
-            inputStyles.colors[color],
-            className,
-          )}
-        >
-          <Image
-            src="/assets/icons/icon-image.svg"
-            alt="사진 첨부 아이콘"
-            priority
-            width={size.img}
-            height={size.img}
-            className="position-center"
-          />
-        </label>
-        <input
-          type="file"
-          ref={ref}
-          accept={accept}
-          className="hidden"
-          id={id}
-          onChange={onChange}
-          {...inputProps}
+  return (
+    <>
+      <label
+        htmlFor={id}
+        className={classNames(
+          'cursor-pointer rounded-full',
+          size.label,
+          inputStyles.colors[color],
+          className,
+        )}
+      >
+        <Image
+          src="/assets/icons/icon-image.svg"
+          alt="사진 첨부 아이콘"
+          priority
+          width={size.img}
+          height={size.img}
+          className="position-center"
         />
-      </>
-    );
-  },
-);
-
-export default ImageInput;
+      </label>
+      <input
+        type="file"
+        accept={accept}
+        className="hidden"
+        id={id}
+        {...inputProps}
+      />
+    </>
+  );
+}
