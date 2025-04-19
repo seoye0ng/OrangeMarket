5d82b428977aafc9ae1091da63241dbacd697730
seoye0ng <bsy0313@gmail.com>
2025-04-19 18:00:09 +0900

    feat: 프로필 수정 컴포넌트에도 계정이름 검증 쿼리훅 적용 및 폼 제출 시 로직 조건 수정(Next-Study-habjung#34) accountname이 기존 값과 다를 경우: 중복 확인을 필수로 함 accountname이 기존 값과 동일할 경우: 중복 확인 없이도 제출 가능

--- src/components/pages/EditAccountPage.tsx
+++ src/components/pages/EditAccountPage.tsx
@@ -2,13 +2,14 @@
 
 import '@/__mock__';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { FormProvider, useForm } from 'react-hook-form';
 
 import { IEditAccountRequest } from '@/api/types/user';
 import ProfileFields from '@/components/common/form/auth-form/ProfileFields';
 import { useHeaderContext } from '@/context/provider/HeaderContext';
 import useNavigate from '@/hooks/useNavigate';
+import useAccountNameValid from '@/queries/auth/useAccountNameValid';
 import useEditAccount from '@/queries/profile/useEditAccount';
 import useProfile from '@/queries/profile/useProfile';
 import AuthService from '@/services/AuthService';
@@ -16,11 +17,15 @@ import AuthService from '@/services/AuthService';
 export default function EditAccountPage({ className }: { className: string }) {
   const accountName = AuthService.getUser() || '';
   const { isHeaderClick, setIsHeaderClick } = useHeaderContext();
+
+  const [initialAccountName, setInitialAccountName] = useState('');
+  const [isAccountAvailable, setIsAccountAvailable] = useState(false);
   const { goTo } = useNavigate();
 
   const { data: userProfile, isLoading } = useProfile(accountName, {
     enabled: !!accountName,
   });
+  const { mutate: accountValid } = useAccountNameValid();
   const { mutate: editAccount, isSuccess: isEditAccountSuccess } =
     useEditAccount();
 
@@ -36,6 +41,14 @@ export default function EditAccountPage({ className }: { className: string }) {
     },
   });
 
+  const checkDuplicate = async () => {
+    const value = methods.getValues('user.accountname');
+    const isValid = await methods.trigger('user.accountname');
+    if (!isValid) return;
+    accountValid({ user: { accountname: value } });
+    setIsAccountAvailable(true);
+  };
+
   // 데이터가 로드되면 폼 값 업데이트. useEffect를 사용해 데이터가 변경될 때만 reset 호출
   useEffect(() => {
     if (!userProfile) return;
@@ -48,6 +61,8 @@ export default function EditAccountPage({ className }: { className: string }) {
         image: userProfile.profile.image,
       },
     });
+
+    setInitialAccountName(userProfile.profile.accountname);
   }, [userProfile, methods]);
 
   // 헤더 클릭 이벤트로 폼 제출 처리
@@ -55,11 +70,30 @@ export default function EditAccountPage({ className }: { className: string }) {
     if (isHeaderClick) {
       methods.handleSubmit((data) => {
         console.log('Data:', data);
+        const currentAccountName = data.user.accountname;
+
+        const isAccountNameChanged = currentAccountName !== initialAccountName;
+
+        // accountname이 변경되었을 경우, 중복 확인을거쳤는지 확인
+        if (isAccountNameChanged && !isAccountAvailable) {
+          // eslint-disable-next-line no-alert
+          alert('계정명을 변경하셨다면 중복 확인을 해주세요.');
+          setIsHeaderClick(false);
+          return;
+        }
+
         editAccount(data);
       })();
       setIsHeaderClick(false);
     }
-  }, [isHeaderClick, methods, setIsHeaderClick, editAccount]);
+  }, [
+    isHeaderClick,
+    methods,
+    setIsHeaderClick,
+    editAccount,
+    initialAccountName,
+    isAccountAvailable,
+  ]);
 
   useEffect(() => {
     if (isEditAccountSuccess) {
@@ -76,7 +110,10 @@ export default function EditAccountPage({ className }: { className: string }) {
     <main className={`pt-[30px] ${className}`}>
       <FormProvider {...methods}>
         <form>
-          <ProfileFields />
+          <ProfileFields
+            isDisabled={methods.watch('user.accountname')?.length < 1}
+            onClick={() => checkDuplicate()}
+          />
         </form>
       </FormProvider>
     </main>
