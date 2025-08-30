89b997f34152730af5db77e74ad6626b1408d58a
seoye0ng <bsy0313@gmail.com>
2025-08-30 13:23:54 +0900

    refactor: 피드 페이지 EmptyState 개선(Next-Study-habjung#72)

--- src/components/common/button/index.ts
+++ src/components/common/button/index.ts
@@ -1,3 +1,4 @@
 export { default as CustomButton } from './Button';
 export { default as IconButton } from './IconButton';
 export { default as TextButton } from './TextButton';
+export { default as TopButton } from './TopButton';
--- src/components/pages/PostFeedPage.tsx
+++ src/components/pages/PostFeedPage.tsx
@@ -1,14 +1,18 @@
 'use client';
 
-import TopButton from '@/components/common/button/TopButton';
+import { CustomButton, TopButton } from '@/components/common/button';
 import EmptyState from '@/components/common/empty-state/EmptyState';
 import InfiniteScrollLoader from '@/components/common/loading/InfiniteScrollLoader';
 import PostItem from '@/components/common/post-item/PostItem';
+import Logo from '@/components/icons/Logo';
 import { POSTS_LIMIT } from '@/constants/infiniteScrollLimits';
 import useInfiniteScroll from '@/hooks/useInfiniteScroll';
+import useNavigate from '@/hooks/useNavigate';
 import useInfinitePostList from '@/queries/post/useInfinitePostList';
 
 export default function PostFeedPage() {
+  const { goTo } = useNavigate();
+
   // 무한 스크롤 구현
   const {
     data,
@@ -35,9 +39,21 @@ export default function PostFeedPage() {
     <main className="h-screen px-4 pb-20 pt-5">
       {data?.pages.every((page) => page.posts.length === 0) ? (
         <EmptyState
-          className="items-center justify-center gap-2 text-center text-gray-500"
-          title="팔로잉 목록이 없습니다."
+          className="items-center justify-center gap-5 text-center text-gray-500"
+          icon={<Logo width={100} color="gray-100" />}
           description="유저를 팔로우하여 피드를 채워보세요!"
+          action={
+            <CustomButton
+              type="button"
+              color="primary"
+              size="l"
+              radius="full"
+              className="px-8"
+              onPress={() => goTo('/search')}
+            >
+              검색하기
+            </CustomButton>
+          }
         />
       ) : (
         data?.pages.map((page) =>
