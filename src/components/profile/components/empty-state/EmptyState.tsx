e0050f10e257594b85243fd49a629b892d11202f
seoye0ng <bsy0313@gmail.com>
2025-08-30 12:26:20 +0900

    refactor: EmptyState 컴포넌트를 확장성 고려해 Slot 기반으로 리팩토링(Next-Study-habjung#72)

--- src/components/profile/components/empty-state/EmptyState.tsx
+++ src/components/profile/components/empty-state/EmptyState.tsx
@@ -1,10 +1,34 @@
 import classNames from 'classnames';
 
 interface EmptyStateProps {
-  message: string;
+  icon?: React.ReactNode;
+  title?: string;
+  description?: string;
+  action?: React.ReactNode; // 버튼
   className?: string;
+  direction?: 'row' | 'col';
 }
 
-export default function EmptyState({ message, className }: EmptyStateProps) {
-  return <div className={classNames(className)}>{message}</div>;
+export default function EmptyState({
+  icon,
+  title,
+  description,
+  action,
+  className,
+  direction = 'col',
+}: EmptyStateProps) {
+  return (
+    <div
+      className={classNames(
+        'flex h-full w-full',
+        direction === 'col' ? 'flex-col' : 'flex-row',
+        className,
+      )}
+    >
+      {icon && <div>{icon}</div>}
+      {title && <p className="font-bold">{title}</p>}
+      {description && <p className="text-gray-500">{description}</p>}
+      {action && <div>{action}</div>}
+    </div>
+  );
 }
