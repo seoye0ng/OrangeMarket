35d5f5b9ee51ecf0a7ef0c4910bc0a34d77af81b
seoye0ng <bsy0313@gmail.com>
2025-09-26 17:56:57 +0900

    feat: 팔로잉, 팔로워 목록 요청 Route Handler 구현(Next-Study-habjung#74)

---
+++ src/app/api/profile/[accountname]/[followListType]/route.ts
@@ -0,0 +1,37 @@
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
+
+export async function GET(
+  req: Request,
+  {
+    params,
+  }: {
+    params: Promise<{
+      accountname: string;
+      followListType: 'following' | 'follower';
+    }>;
+  },
+) {
+  const { accountname, followListType } = await params;
+
+  const { searchParams } = new URL(req.url);
+  const limit = searchParams.get('limit');
+  const skip = searchParams.get('skip');
+
+  const token = cookies().get('token')?.value;
+
+  const res = await fetch(
+    `${process.env.BACKEND_BASE_URL}/profile/${accountname}/${followListType}?limit=${limit}&skip=${skip}`,
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        ...(token ? { Authorization: `Bearer ${token}` } : {}),
+      },
+      cache: 'no-store',
+    },
+  );
+
+  const data = await res.json();
+  return NextResponse.json(data);
+}
