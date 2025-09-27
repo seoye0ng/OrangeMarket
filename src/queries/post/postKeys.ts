51cbffec4e9009f9d1ebb627050a8562979a4bd9
seoye0ng <bsy0313@gmail.com>
2025-09-27 14:25:22 +0900

    feat: 요청(좋아요/취소) 성공 시 invalidateQueries로 관련 캐시 리프레시(Next-Study-habjung#76)

--- src/queries/post/postKeys.ts
+++ src/queries/post/postKeys.ts
@@ -1,8 +1,7 @@
 export const postKeys = {
   all: ['post'] as const,
-  listAll: ({ limit }: { limit: number }) =>
-    [...postKeys.all, 'list', { limit }] as const,
-  list: ({ limit, accountName }: { limit: number; accountName: string }) =>
-    [...postKeys.all, { limit, accountName }] as const,
+  listAll: () => [...postKeys.all, 'list'] as const,
+  list: ({ accountName }: { accountName: string }) =>
+    [...postKeys.all, { accountName }] as const,
   detail: (id: string) => [...postKeys.all, 'detail', id] as const,
 };
